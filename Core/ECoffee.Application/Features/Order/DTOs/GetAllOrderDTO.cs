08b8bccd736b47a23d9a101298f679f9bd3855c4
Zafer Avcı <zaferavci89@gmail.com>
2023-04-01 14:40:52 +0300

    Order DTO Added

---
+++ Core/ECoffee.Application/Features/Order/DTOs/GetAllOrderDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace ECoffee.Application.Features.Order.DTOs
+{
+    public class GetAllOrderDTO
+    {
+        public int Id { get; set; }
+        public string CustomerName { get; set; }
+        public List<string> ProductNames { get; set; }
+    }
+}
