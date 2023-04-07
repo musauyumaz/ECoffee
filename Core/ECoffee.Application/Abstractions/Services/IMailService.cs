ced2d474620f8d99be973556a34385986ce0bce8
Musa UYUMAZ <113431738+musauyumaz@users.noreply.github.com>
2023-04-07 23:48:23 +0300

    Mail Service Added

---
+++ Core/ECoffee.Application/Abstractions/Services/IMailService.cs
@@ -0,0 +1,8 @@
+﻿namespace ECoffee.Application.Abstractions.Services
+{
+    public interface IMailService
+    {
+        Task SendMailAsync(string to,string subject,string body,bool isBodyHtml = true);
+        Task SendMailAsync(string[] tos,string subject,string body,bool isBodyHtml = true);
+    }
+}
