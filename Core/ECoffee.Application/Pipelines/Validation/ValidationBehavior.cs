f61bbd42e4ba2da604e2327a5deed5167d2c15d7
Musa UYUMAZ <113431738+musauyumaz@users.noreply.github.com>
2023-04-25 23:52:28 +0300

    Validation Added

---
+++ Core/ECoffee.Application/Pipelines/Validation/ValidationBehavior.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using MediatR;
+
+namespace ECoffee.Application.Pipelines.Validation
+{
+    public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+        public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            ValidationContext<TRequest> context = new(request);
+            List<ValidationFailure> failures = _validators
+                                      .Select(validator => validator.Validate(context))
+                                      .SelectMany(result => result.Errors)
+                                      .Where(failure => failure != null)
+                                      .ToList();
+            if (failures.Any()) throw new ValidationException(failures);
+            return next();
+        }
+    }
+}
--- Core/ECoffee.Application/ServiceRegistration.cs
+++ Core/ECoffee.Application/ServiceRegistration.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using ECoffee.Application.Pipelines.Validation;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 
 namespace ECoffee.Application
@@ -8,6 +11,8 @@ namespace ECoffee.Application
         public static void AddApplicationServices(this IServiceCollection services)
         {
             services.AddMediatR(configuration => configuration.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+            services.AddTransient(typeof(IPipelineBehavior<,>),typeof(ValidationBehavior<,>));
+            services.AddValidatorsFromAssembly(typeof(ServiceRegistration).Assembly);
         }
     }
 }
